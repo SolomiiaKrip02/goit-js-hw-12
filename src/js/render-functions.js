a3b5ab2e8dfd7d2654e0af6735afee58ffb44be2
SolomiiaKrip02 <solomiya.kr08@gmail.com>
2025-08-25 02:24:56 +0300

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,56 +1,32 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const gallery = document.querySelector('.gallery');
-const loader = document.querySelector('.loader');
-const loadMoreBtn = document.querySelector('.load-more');
-
-let lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-  showCounter: true,
-  animationSpeed: 250,
-});
+let lightbox;
 
 export function createGallery(images) {
   const markup = images
     .map(
-      img => `
-        <div class="photo-card">
-          <a href="${img.largeImageURL}">
-            <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
-          </a>
-          <div class="info">
-            <p><b>Likes:</b> ${img.likes}</p>
-            <p><b>Views:</b> ${img.views}</p>
-            <p><b>Comments:</b> ${img.comments}</p>
-            <p><b>Downloads:</b> ${img.downloads}</p>
-          </div>
+      image => `
+      <a class="gallery-item" href="${image.largeImageURL}">
+        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        <div class="info">
+          <p><b>Likes:</b> ${image.likes}</p>
+          <p><b>Views:</b> ${image.views}</p>
+          <p><b>Comments:</b> ${image.comments}</p>
+          <p><b>Downloads:</b> ${image.downloads}</p>
         </div>
-      `
+      </a>`
     )
     .join('');
-
   gallery.insertAdjacentHTML('beforeend', markup);
-  lightbox.refresh();
-}
 
-export function clearGallery() {
-  gallery.innerHTML = '';
-}
-
-export function showLoader() {
-  loader.classList.remove('hidden');
-}
-
-export function hideLoader() {
-  loader.classList.add('hidden');
-}
-
-export function showLoadMoreButton() {
-  loadMoreBtn.classList.remove('hidden');
-}
-
-export function hideLoadMoreButton() {
-  loadMoreBtn.classList.add('hidden');
+  // ❗ Ініціалізація або оновлення SimpleLightbox
+  if (lightbox) {
+    lightbox.refresh();
+  } else {
+    lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+  }
 }
