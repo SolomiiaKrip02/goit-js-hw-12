3617fa4d8c0a770587c7a5cb8ef0bf036e31f934
SolomiiaKrip02 <solomiya.kr08@gmail.com>
2025-08-25 02:24:52 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,17 +3,17 @@ import axios from 'axios';
 const API_KEY = '51941180-25d97e4841ef256248244227c';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function getImagesByQuery(query, page) {
-  const params = {
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    page: page,
-    per_page: 15,
-  };
-
-  const response = await axios.get(BASE_URL, { params });
+export default async function fetchData(query, page = 1) {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: page,
+      per_page: 15,
+    },
+  });
   return response.data;
 }
