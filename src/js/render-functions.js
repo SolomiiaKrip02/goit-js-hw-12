e94096580d8c97e192c1efa582fd4fc0e27e17d8
SolomiiaKrip02 <solomiya.kr08@gmail.com>
2025-08-25 18:27:38 +0300

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,37 +1,34 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 const loader = document.querySelector('.loader');
+const gallery = document.querySelector('.gallery');
 
-let lightbox;
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 export function createGallery(images) {
   const markup = images
     .map(
-      image => `
-      <a class="gallery-item" href="${image.largeImageURL}">
-        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+      img => `
+      <li class="gallery-item">
+        <a href="${img.largeImageURL}">
+          <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
+        </a>
         <div class="info">
-          <p><b>Likes:</b> ${image.likes}</p>
-          <p><b>Views:</b> ${image.views}</p>
-          <p><b>Comments:</b> ${image.comments}</p>
-          <p><b>Downloads:</b> ${image.downloads}</p>
+          <span>Likes: ${img.likes}</span>
+          <span>Views: ${img.views}</span>
+          <span>Comments: ${img.comments}</span>
+          <span>Downloads: ${img.downloads}</span>
         </div>
-      </a>`
+      </li>`
     )
     .join('');
   gallery.insertAdjacentHTML('beforeend', markup);
-
-  if (lightbox) {
-    lightbox.refresh();
-  } else {
-    lightbox = new SimpleLightbox('.gallery a', {
-      captionsData: 'alt',
-      captionDelay: 250,
-    });
-  }
+  lightbox.refresh();
 }
 
 export function clearGallery() {
